20982bc48877757e203ebc27fe9d00d0acca7ef0
firoj khan <firojkcsjm0805@gmail.com>
2023-08-04 20:45:15 +0530

    Added lazy loading component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Accordion from './components/Accordion';
-import TodoApp from './components/TodoApp';
+import React, { Suspense } from "react";
+import "./App.css";
+import Accordion from "./components/Accordion";
+import TodoApp from "./components/TodoApp";
+
+const ComponentRender = React.lazy(() => import("./components/LazyLoading"));
+
 
 function App() {
   return (
-    <div style={{paddingTop: "2.5rem"}}>
+    <div style={{ paddingTop: "2.5rem" }}>
+      <Suspense fallback="Loading">
+        <ComponentRender />
+      </Suspense>
       <Accordion />
       <TodoApp />
     </div>
